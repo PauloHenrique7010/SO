87d05600441ae0ede07165896e035e4cd40b73d8
Paulo Henrique da Silva <49278654+PauloHenrique7010@users.noreply.github.com>
2019-05-14 07:35:09 -0300

    Paulo_Henrique_EX5_SO1B.py
    Inicio do EX5

---
+++ Paulo_Henrique_EX5_SO1B.py
@@ -0,0 +1,30 @@
+'''Implementar os algoritmos de escalonamento:
+1.	FCFS
+2.	SJF
+3.	SRTF
+4.	Round Robin
+5.	Multinível
+a.	Primeiro nível RR
+b.	Segundo nível FCFS
+Para todos os algoritmos, deverão constar um menu para as seguintes entradas.
+•	Quantidade de processos para simular
+•	Tamanho do Burst do processo (manual ou arquivo)
+•	Tempo de chegada, quando necessário
+•	Prioridade, quando necessário
+•	Quantum de tempo, quando necessário
+Resultados
+•	Lista dos processos com os seus burst, prioridades e tempo de chegada
+•	Exibir o Waiting Time e Turnaround Time de cada processo no algoritmo
+•	Exibir o Waiting Time médio
+•	Exibir o Turnaround Time médio
+Padrão Arquivo de Entrada de Dados
+@nomeprocesso;burst;tempochegada;prioridade;quantum&
+Arquivo para o FCFS
+@p1;24;0;0;0&
+@p2;3;0;0;0&
+Arquivo para o STRF
+@p1;8;2;0;0&
+@p2;4;4;0;0&
+Arquivo para o RR e multinível
+@p1;80;0;0;10&
+@p1;60;0;0;10&'''
