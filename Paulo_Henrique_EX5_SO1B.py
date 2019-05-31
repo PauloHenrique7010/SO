c5067807684da8035515b8db26ed7b6e965e61fc
Paulo Henrique da Silva <49278654+PauloHenrique7010@users.noreply.github.com>
2019-05-30 21:13:25 -0300

    - FCFS completo mas ver se vai ter esquema de prioridade

--- Paulo_Henrique_EX5_SO1B.py
+++ Paulo_Henrique_EX5_SO1B.py
@@ -34,12 +34,10 @@ import random
 import operator
 
 class Processo(object):
-  def __init__(self, nome,burst,tcheg, taround,tresposta):
+  def __init__(self, nome,burst,tcheg,):
       self.nome = nome
       self.burst = burst
       self.tcheg = tcheg
-      self.taround = taround
-      self.tresposta = tresposta
   
   def setNome(nome):
     self.nome = nome
@@ -49,50 +47,55 @@ class Processo(object):
   
   def setTcheg(tcheg):
     self.tcheg = tcheg
+lista = []
+def montaListaAutomatica(arquivo):
+  for x in range(len(arquivo)):      
+      processo = arquivo[x]
+      processo = processo.split('@')[1]
+      processo = processo.split('&')[0]
+      processo = processo.split(';')
 
-  def setTcheg(taround):
-    self.taround = taround
-  def setTresposta(tresposta):
-    self.tresposta = tresposta
+      nomeProcesso = processo[0] #p1
+      nomeProcesso = nomeProcesso[1:]#pega apartir o p.. p2 -> 2, p23 -> 23
+      burstProcesso = processo[1]
+      tChegProcesso = processo[2]     
+      proc = Processo(x,burstProcesso,tChegProcesso)
+      lista.append(proc)
+  return lista
+      
+def imprimirResultado(lista,tipoEscalonamento):
+  tAround = 0
+  tEspera = 0
+  tResposta = 0
+  turnMedia = 0
+  esperaMedia = 0
+  if tipoEscalonamento == "FCFS":
+    
+    for x in lista:
+      if int(x.nome) == 0: #caso seja o primeiro processo, tempo de espera, e o tempo de chegada
+        tEspera = x.tcheg
+      else:
+        tEspera = tAround-int(x.tcheg)
+      tAround += (int(x.burst)-int(x.tcheg))
+      tResposta = tEspera #no fcfs, tempo de resposta é o mesmo de espera
+      turnMedia += tAround
+      esperaMedia += int(tEspera)
+      
+      print(int(x.nome)+1,"\t\t",x.burst,"\t",x.tcheg,"\t\t",tAround,"\t\t",tResposta,"\t\t",tEspera)
+    print('\nMédias\n')
+    print(f'TurnAround: {turnMedia/len(lista)}')
+    print(f'Espera: {esperaMedia/len(lista)}')
 
 def FCFS(): 
-  c = input("Burst manual ou arquivo(M/A)?\n ")  
-
+  c = input("Burst manual ou arquivo(M/A)?\n ")
   if c == "A" or c == "a":
     #burst e tempo de chegada automático
-    '''arquivo = "fcfs.txt"
+    arquivo = "fcfs.txt"
     arquivo = open(arquivo)
     arquivo = arquivo.read()
-    arquivo = arquivo.split()'''
-    arquivo = ['@p1;15;0&','@p2;3;0&','@p3;4;0&','@p4;8;2&']
-    print(arquivo)
+    arquivo = arquivo.split()
     lista = []
-
-    for x in range(len(arquivo)):
-      prepara = arquivo[x]
-      prepara = prepara.split('@')
-      separa = prepara[1]
-      separa = separa.split('&')
-      gol = separa[0]
-      gol = gol.split(';')
-      processo = gol[0]
-      processo = int(processo[1:])
-      burst = int(gol[1])
-      tchegada = int(gol[2])
-
-      if x == 0:
-        tresposta = tchegada
-        taround = burst
-        tresposta = tchegada
-      else:
-        tresposta = taround - tchegada
-        taround += burst-tchegada
-        
-      proc = Processo(x,burst,tchegada,taround,tresposta)
-      lista.append(proc)
-      
-      
-      
+    lista = montaListaAutomatica(arquivo)      
   else:
     lista = []
     n = int(input("Qual a quantidade de processos?\n"))
@@ -100,17 +103,13 @@ def FCFS():
     for i in range(n):
         b = int(input("P" + str(i+1) +" Burst:"))
         t = int(input("P" + str(i+1) +" Tempo de Chegada:")) 
-        if i == 0:
-          tresposta = t
-          taround = b
-          tresposta = t
-        else:
-          tresposta = taround - t
-          taround += b-t
-          
-
-        proc = Processo(i,b,t,taround,tresposta)
+        proc = Processo(i,b,t)
         lista.append(proc)
+  lista.sort(key = operator.attrgetter("tcheg"), reverse = False) #FCFS -> ordena por tempo de chegada
+  print ("Todos os processos...")
+  print ("Processo\tT.Burst\tT.Chegada\tTurnAround\tT.Resposta\tT.Espera")
+  imprimirResultado(lista,"FCFS")
+  
   
 def SJF(): #@nomeprocesso;burst;tempochegada;prioridade;quantum& 
   c = input("Burst manual ou arquivo(M/A)?\n ")  
@@ -186,10 +185,39 @@ def SJF(): #@nomeprocesso;burst;tempochegada;prioridade;quantum&
       print("\t",i.nome,"\t\t\t",i.burst,"\t\t\t",i.tcheg,"\t\t\t",i.taround,"\t\t\t",i.tresposta,"\t\t\t",i.tresposta)
     contador +=1
 
+def SRTF():
+  c = input("Burst manual ou arquivo(M/A)?\n ")
+  tempoPassado = 0
+  nomeAtual = 0
+  burstAtual = 0
+  tchegAtual = 0
+ 
+  def vetorTempo(processoQueEstaAgora,tempopassado):
+    for x in range(len(lista)):
+      if x == processoQueEstaAgora:
+        nomeAtual = lista[x].nome
+        burstAtual = lista[x].burst
+        tchegAtual = lista[x].tcheg
+      
+      if x != processoQueEstaAgora:
+        if (burstAtual-tchegAtual) < (lista[x].burst-lista[x].tcheg):
+          lista[nomeAtual].burst-=1
+          tempoPassado+=1
+        else:
+          vetorTempo(lista[x],int(tempoPassado))
+          
+      
+      
+  
 
-def preparaProcessos(arquivo):
+  
+  if c == "A" or c == "a":
     #burst e tempo de chegada automático
-    
+    '''arquivo = "fcfs.txt"
+    arquivo = open(arquivo)
+    arquivo = arquivo.read()
+    arquivo = arquivo.split()'''
+    arquivo = ['@p1;15;0&','@p2;2;2&','@p3;3;5&','@p2;2;7&']
     print(arquivo)
     lista = []
 
@@ -215,50 +243,8 @@ def preparaProcessos(arquivo):
         
       proc = Processo(x,burst,tchegada,taround,tresposta)
       lista.append(proc)
-    return lista
-    
-
-def RR():
-    c = input("Burst manual ou arquivo(M/A)?\n ")
-
-def SRTF():
-  c = input("Burst manual ou arquivo(M/A)?\n ")
-  tempoPassado = 0
-  atual = 0
-  lista = []
-  
- 
-  def vetorTempo(processoQueEstaAgora,tempopassado):
-    for x in range(len(lista)): #pega o processo que esta
-        if lista[x].nome == processoQueEstaAgora:
-            atual = lista[x].nome
-            break
-    print(f'P{lista[atual].nome};{lista[atual].burst};{lista[atual].tcheg}')
-
-    
-    while lista[atual].burst != 0:
-        for x in range(len(lista)):
-            if lista[x].nome != lista[atual].nome: #nao pegar o mesmo processo
-                pass
-            break
-        break
-                    
-    
-  
-
-  
-  if c == "A" or c == "a":
-    '''arquivo = "fcfs.txt"
-    arquivo = open(arquivo)
-    arquivo = arquivo.read()
-    arquivo = arquivo.split()'''
-    arquivo = ['@p0;8;0&','@p1;2;2&','@p2;3;5&','@p3;2;7&']
-    lista = preparaProcessos(arquivo) 
-    
     lista.sort(key = operator.attrgetter("tcheg"), reverse = False)
-    for x in lista:
-        vetorTempo(x.nome,0)
-        break
+    vetorTempo(0,0)
     
     
       
